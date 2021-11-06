23a691adea81a82de72a5cca593a1486edb44f0d
sehajbajaj <sehajbajaj2001@gmail.com>
2021-11-06 20:09:30 +0530

    add index.html App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,72 @@
+
+import axios from 'axios';
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import CityComponent from './modules/CityComponent';
+import WeatherComponent from './modules/WeatherInfoComponent';
+import { ChakraProvider, Box, Heading, extendTheme, Grid, Button } from '@chakra-ui/react';
+
+import {  useState } from 'react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+const API_KEY = 'a0aa252406836b8d60ee043b85fae6c1';
 
 function App() {
+  const theme = {
+    styles: {
+      global: {
+        'html, body': {
+          m: '0',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          h: '100vh',
+          w: '100%',
+          // color:'black'
+        },
+      },
+    },
+  };
+  const customTheme = extendTheme(theme);
+
+  const [city, changeCity] = useState();
+  const [weather, changeWeather] = useState();
+
+  const fetchWeather = async e => {
+    e.preventDefault();
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
+    changeWeather(response.data);
+  };
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
+    <ChakraProvider theme={customTheme}>
+      <Grid p={3} mr = "0">
           <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+          
         </Grid>
+      <Box
+        d="flex"
+        flexDirection="column"
+        m="auto"
+        textAlign="center"
+        shadow="dark-lg"
+        p="20px 10px"
+        borderRadius="4px"
+        w="450px"
+        bg="gray.200"
+      >
+        <Heading fontSize="22px" fontFamily="Zen Kurenaido" color="black">
+          React Weather App
+        </Heading>
+        {weather ? (
+          <WeatherComponent weather = {weather}/>
+        ) : (
+          <CityComponent changeCity={changeCity} fetchWeather={fetchWeather} />
+        )}
+      
       </Box>
+      
+      
     </ChakraProvider>
   );
 }
